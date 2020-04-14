70fbece9a53d098dc196d409c1bc21f9df53d87c
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-04-14 17:47:52 -0500

    ajuste del if

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hola ' + req.query.nombre+'!</h1>');
+    var data = ""
+    data = req.query.nombre ? req.query.nombre : 'desconocido'
+   res.send( '<h1>Hola ' + data +'!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
